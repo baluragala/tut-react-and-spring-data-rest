65f28ba91e8625334f942ce8c5585e7e20d33a4d
Bala Krishna Ragala <bala.ragala@knowledgehut.co>
2023-01-08 00:46:27 +0530

    Added sample test case

---
+++ basic/src/test/java/com/greglturnquist/payroll/test/HomeControllerTest.java
@@ -0,0 +1,40 @@
+package com.greglturnquist.payroll.test;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.greglturnquist.payroll.ReactAndSpringDataRestApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+@SpringBootTest(classes = ReactAndSpringDataRestApplication.class)
+public class HomeControllerTest extends AbstractTestNGSpringContextTests {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @BeforeClass
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void testEmployee() throws Exception {
+        mockMvc.perform(get("/api/employees")).andExpect(status().isOk())
+                .andExpect(content().contentType("application/hal+json"));
+
+
+    }
+
+}
